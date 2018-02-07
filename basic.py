cdef05e92ddf9b869b47b9b873dcf4a13424137b
fancyzero <fancyzero@live.cn>
2018-02-07 18:02:40 +0800

    add gradient function

--- basic.py
+++ basic.py
@@ -2,15 +2,22 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from decimal import Decimal
+import functools
 
-def field_func(x,y):
-    return np.array((np.sin(y),np.cos(x)))
+def scalar_field_func(x,y):
+    return np.sin(x)-np.cos(y)
+
+def gradient(func, x,y,delta=0.00001):
+    f = func(x,y)
+    partial_x = func(x+delta,y)-f
+    partial_y = func(x,y+delta)-f
+    return np.array((partial_x/delta, partial_y/delta))
 
 def partial_derivative(func, x,y,respectto, delta=(0.00001)):
     if (respectto == "x"):
-        return ((func(x + delta, y) - func(x, y)) / delta)
+        return ((func(x=x + delta, y=y) - func(x=x, y=y)) / delta)
     if (respectto == "y" ):
-        return ((func(x, y+delta) - func(x, y)) / delta)
+        return ((func(x=x, y=y+delta) - func(x=x, y=y)) / delta)
 
 
 def divergence(func,x, y):
@@ -31,12 +38,11 @@ def show_vector_field(func,x,y,title=""):
     f=np.ndarray((len(x),len(y),2))
     for i,vx in enumerate(x):
         for j,vy in enumerate(y):
-            pp = field_func(vx, vy)
-            f[i, j] = pp
+            f[i, j] = func(x=vx, y=vy)
     r = np.dsplit(f,2)
-    u = np.squeeze(r[1])
-    v = np.squeeze(r[0])
-    plt.quiver(x,y,u, v, units="width")
+    u = np.squeeze(r[0]).T
+    v = np.squeeze(r[1]).T
+    plt.quiver(x, y, u, v, units="width")
     plt.title(title)
     plt.show()
 
@@ -46,10 +52,11 @@ def show_scalar_field(field,title=""):
     plt.title(title)
     plt.show()
 
-row = np.arange(-6,6,0.4)
-column = np.arange(-6,6,0.4)
-show_vector_field(field_func,row,column,"field")
-show_scalar_field(divergence(field_func,row,column),"divergence")
-show_scalar_field(curl(field_func,row,column),"curl")
+row = np.arange(-3.14,3.14,0.2)
+column = np.arange(-3.14,3.14,0.2)
+func = functools.partial(gradient,func=scalar_field_func)
+show_vector_field(func,row,column,"field")
+show_scalar_field(divergence(func,row,column),"Laplacian") #Laplacian ( divergence of gradient of a scalar field )
+show_scalar_field(curl(func,row,column),"curl") # curl of gradient it should be 0
 
 
